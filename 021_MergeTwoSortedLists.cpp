5bff29da318d0b92c75f53317a1ecba777f444d3
IsagiYoichiDSA <toshikikai612@gmail.com>
2025-01-09 23:50:49 +0530

    Added MergeTwoSortedLists

---
+++ 021_MergeTwoSortedLists.cpp
@@ -0,0 +1,27 @@
+// Iterative
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        ListNode *st = new ListNode(0);
+        ListNode *res = st;
+
+        while (list1 && list2)
+        {
+            if (list1->val < list2->val)
+            {
+                res->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                res->next = list2;
+                list2 = list2->next;
+            }
+            res = res->next;
+        }
+        res->next = list1 ? list1 : list2;
+        return st->next;
+    }
+};
