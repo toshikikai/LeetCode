544cb2e3fddea153c4fc68d0a12473b63724f399
IsagiYoichiDSA <toshikikai612@gmail.com>
2025-01-09 23:53:49 +0530

    Update 021_MergeTwoSortedLists.cpp

--- 021_MergeTwoSortedLists.cpp
+++ 021_MergeTwoSortedLists.cpp
@@ -25,3 +25,20 @@ public:
         return st->next;
     }
 };
+
+// Recursive
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        if (!list1 || !list2)
+        {
+            return list1 ? list1 : list2;
+        }
+        if (list1->val > list2->val)
+            swap(list1, list2);
+        list1->next = mergeTwoLists(list1->next, list2);
+        return list1;
+    }
+};
